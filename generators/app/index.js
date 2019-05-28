aaf103c5558faae62f9a1b61447f9612a87f4a9d
sudokar <sudhakarms@yahoo.co.in>
2019-05-28 15:38:03 +0100

    Updated Readme.md and new release

--- generators/app/index.js
+++ generators/app/index.js
@@ -9,7 +9,7 @@ module.exports = class extends Generator {
 
   async prompting() {
     this.log(
-      yosay('Welcome to the tf-module generator v0.4.0!')
+      yosay('Welcome to the tf-module generator v0.5.1!')
     );
 
     this.answers = await this.prompt([{
