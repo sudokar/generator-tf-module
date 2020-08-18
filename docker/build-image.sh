db27156b9e95053114e8e3ea958be15e2257aeac
Wes Dean <wdean@flexion.us>
2020-08-18 07:19:28 -0400

    Add comments and a check for `jq`
    If the `jq` command isn't located (via `which`) on the system, display
    a helpful message to STDERR and exit with a failure.  Why?  Well, this
    is a new requirement and I, personally, would rather see an explanation
    why my previously working builds are now failing rather than either
    having the build fail with an odd error message or, possibly worse, have
    it "succeed" with garbage build arguments.
    
    Also, I put a comment in at the top of the script explaining the purpose
    of the script and why it's likely most users don't need to build the
    image themselves.

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
 
+# This script builds the Docker image for Sudokar's generator-tf-module
+# tool.  Generally speaking, it's often sufficient to grab this image
+# from Docker Hub rather than building it separately.
+#
+# To pull from Docker Hub:
+#   docker pull sudokar/generator-tf-module:latest
+#
+# For more about the image on Docker Hub:
+#   https://hub.docker.com/r/sudokar/generator-tf-module
+
+# since this is a change from previous behavior (the 'jq' requirement is
+# new), make sure to check we can locate the newly required command; if
+# not, provide a helpful message (to STDERR) to the user so they can 
+# remedy the problem, and then exit with an error code so it's clear
+# that the build was not successful.
+if ! which jq > /dev/null ; then
+  echo "This build script now requires 'jq' to build." 1>&2
+  echo "Find it at: https://stedolan.github.io/jq/" 1>&2
+  exit 1
+fi
+
 # you may wish to change these, depending on how you're building / testing
 OWNER="${OWNER:-sudokar}"
 NAME="${NAME:-$(cat ../package.json | jq -r '.name')}"
