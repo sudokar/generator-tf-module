4d8cb6f648519530b7b7148433936b7f85d7d724
Wes Dean <wdean@flexion.us>
2020-08-18 06:57:30 -0400

    Document script and Docker build context
    Comments were to the script to make it more readable, especially to
    those who wish to use it to futher development efforts.
    
    Also, because I'm a pedant, I made sure to send the location of the
    Docker file (hence, build context) along to the `docker build` command
    so that it can be executed from anywhere.
    
    Lastly, I set `xtrace` so that the `docker build` command is displayed
    before it's executed; that way, one can audit the command when debugging
    a build.

--- docker/build-image.sh
+++ docker/build-image.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+# you may wish to change these, depending on how you're building / testing
 OWNER="${OWNER:-sudokar}"
-VERSION="${VERSION:-$(cat ../package.json | jq -r '.version')}"
 NAME="${NAME:-$(cat ../package.json | jq -r '.name')}"
+
+# this grabs the version from package.json; you probably don't need to change
+VERSION="${VERSION:-$(cat ../package.json | jq -r '.version')}"
+
+# these are grabbed automatically; you really don't want the change
 BUILD_DATE="${BUILD_DATE:-$(date -u +"%Y-%m-%dT%H:%M:%SZ")}"
 VCS_REF="${VCS_REF:-$(git rev-parse --short HEAD)}"
+DOCKER_ROOT="${DOCKER_ROOT:-$(git rev-parse --show-toplevel)/docker}"
 
+# make sure we show the `docker build` command before it's executed
+set -o xtrace
+
+# perform the build using the arguments previously supplied
 docker build --build-arg BUILD_DATE=$BUILD_DATE \
              --build-arg VCS_REF=$VCS_REF \
-             --build-arg VERSION=$VERSION -t $OWNER/$NAME:$VERSION .
+             --build-arg VERSION=$VERSION \
+             --tag $OWNER/$NAME:$VERSION \
+             "${DOCKER_ROOT}"
 
-docker tag $OWNER/$NAME:$VERSION $OWNER/$NAME:latest
