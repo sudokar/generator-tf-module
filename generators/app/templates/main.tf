700c5bd974c88eee9ad48643ef1e9d77710cd4e8
Adrienne Cohea <adriennecohea@gmail.com>
2020-11-24 20:10:38 -0800

    Add cloud-config

---
+++ generators/app/templates/main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_instance" "example" {
+  name         = "example-yeoman-generator"
+  machine_type = "e2-medium"
+  zone         = "us-west1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+    }
+  }
+
+  metadata_startup_script = templatefile("${path.module}/cloud-config.yaml")
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
